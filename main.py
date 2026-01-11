db98ab233904f3942a50d2975b51eb98fe769a3e
Jack Tubby <jack200034@hotmail.com>
2026-01-11 23:55:31 +0000

    Setup simple overlay

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
+import sys
 import gi
 gi.require_version('Gtk', '4.0')
-from gi.repository import Gtk
+gi.require_version('Adw', '1')
+gi.require_version('Gtk4LayerShell', '1.0')
+from gi.repository import Gtk, Adw, Gtk4LayerShell, GLib
 
-def on_activate(app):
-    win = Gtk.ApplicationWindow(application=app)
-    win.present()
 
-app = Gtk.Application()
-app.connect('activate', on_activate)
+class MainWindow(Gtk.ApplicationWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        Gtk4LayerShell.init_for_window(self)
+        Gtk4LayerShell.set_layer(self, Gtk4LayerShell.Layer.OVERLAY)
+        
+        GLib.timeout_add(3000, self.close_window)
+    
+    def close_window(self):
+        self.close()
+        return False
 
-app.run(None)
+
+class MyApp(Adw.Application):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.connect('activate', self.on_activate)
+
+    def on_activate(self, app):
+        self.win = MainWindow(application=app)
+        self.win.present()
+
+app = MyApp(application_id="com.example.GtkApplication")
+app.run(sys.argv)
